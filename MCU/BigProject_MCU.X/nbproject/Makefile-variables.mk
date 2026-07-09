13c914a0b5256d0002dabdc70e608ba492316dd7
Gia Khang <giakhangt894@gmail.com>
2026-07-09 20:10:43 +0700

    MCU: Update MPLA project

---
+++ MCU/BigProject_MCU.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=BigProject_MCU.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/BigProject_MCU.X.production.hex
